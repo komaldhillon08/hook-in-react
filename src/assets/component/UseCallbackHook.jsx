047faa9edea1404ec42758faa177dc6ce54bb378
sanjay <sanjay8195850041@gmail.com>
2025-07-24 17:02:20 +0530

    add useCallback hook in react and sec the file in main page

--- src/assets/component/UseCallbackHook.jsx
+++ src/assets/component/UseCallbackHook.jsx
@@ -1,8 +1,43 @@
+import { useCallback, useState , memo} from "react"
+
+// create components start
+const Button = memo(function Button({ onclick, children }) {
+    console.log(`Button ${children} rendered`);
+
+    return (
+        <>
+            <button onClick={onclick}>{children}</button>
+
+        </>
+    )
+})
+// components end 
+
 export default function UseCallbackHook() {
+
+    const [count, setCount] = useState(0)
+
+    const increment = useCallback(() => {
+        console.log(" inside  increment>>>>>>>>>>> + 1");
+        setCount((prev) => prev + 1)
+
+    }, [] )
+    const decrement = useCallback(() => {
+        console.log(" inside  decrement>>>>>>>>>>> + 1");
+        setCount((prev) => prev - 1)
+    },[]);
+
+
+
+
     return (
         <>
             <h2>UseCallbackHook</h2>
-            
+
+            <Button>{count}</Button>
+            <Button onclick={increment}>Increment</Button>
+            <Button onclick={decrement}>Decrement</Button>
+
         </>
     )
 }
